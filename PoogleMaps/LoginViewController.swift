3fe47b59073c0786d838de60169f4ff5f630a112
Patrick Hansen <pat@patricks-macbook-pro.local>
2016-04-20 11:36:08 -0400

    Login with Firebase.

--- PoogleMaps/LoginViewController.swift
+++ PoogleMaps/LoginViewController.swift
@@ -7,34 +7,63 @@
 //
 
 import UIKit
+import Firebase
 
-class LoginViewController: UIViewController {
+class LoginViewController: UIViewController, UITextFieldDelegate {
 
     @IBOutlet weak var emailTextField: UITextField!
     @IBOutlet weak var passwordTextField: UITextField!
     
+    // Create a reference to a Firebase location
+    var root = Firebase(url:"https://poogle-maps.firebaseio.com/")
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        emailTextField.delegate = self
+        passwordTextField.delegate = self
     }
     
+    //
+    // MARK: - IBActions
+    //
     
     @IBAction func signInButtonTouched(sender: AnyObject) {
-    
+        /*
+        if emailTextField.text == "" || passwordTextField.text == "" {
+            return
+        }*/
+        
+        root.authUser(emailTextField.text, password: passwordTextField.text, withCompletionBlock: { error, authData in
+            
+            if error != nil {
+                let alert = UIAlertController(title: "Whoops", message: "Error logging in: \(error.localizedDescription)", preferredStyle: UIAlertControllerStyle.Alert)
+                alert.addAction(UIAlertAction(title: "Ok", style: .Default, handler: nil))
+                self.presentViewController(alert, animated: true, completion: nil)
+            } else {
+                print("success")
+            }
+            
+        })
+        
     }
 
     @IBAction func cancel(sender: AnyObject) {
         self.dismissViewControllerAnimated(true, completion: nil)
     }
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    
+    //
+    // MARK: - TextFieldDelegate
+    //
+    
+    func textFieldShouldReturn(textField: UITextField) -> Bool {
+        if textField.tag == 1 {
+            textField.resignFirstResponder()
+            passwordTextField.becomeFirstResponder()
+        } else  if textField.tag == 2 {
+            textField.resignFirstResponder()
+        }
+        return true
     }
-    */
-
+    
 }
